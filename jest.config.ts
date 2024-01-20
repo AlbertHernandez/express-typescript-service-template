b8e8b5068d8e1fb6b61307444390f35cb3c0210d
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-01-20 10:04:52 +0100

    chore: remove ts-jest to use fully swc also for ts paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
-import type { JestConfigWithTsJest } from "ts-jest";
-import { pathsToModuleNameMapper } from "ts-jest";
+import fs from "fs-extra";
+import type { Config } from "jest";
 
-import { compilerOptions } from "./tsconfig.json";
+const swcConfig = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, "utf-8"));
 
-const config: JestConfigWithTsJest = {
+const config: Config = {
   transform: {
-    "^.+\\.(t|j)s$": "@swc/jest",
+    "^.+\\.(t|j)s$": ["@swc/jest", swcConfig],
   },
   testEnvironment: "node",
   cacheDirectory: ".tmp/jestCache",
@@ -13,8 +13,6 @@ const config: JestConfigWithTsJest = {
   collectCoverageFrom: ["src/**/*.ts"],
   setupFiles: ["<rootDir>.jest/set-env-vars.ts"],
   clearMocks: true,
-  modulePaths: [compilerOptions.baseUrl],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
 
 export default config;
